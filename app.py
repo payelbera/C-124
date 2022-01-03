f80bc25ac2f51ce179a27a0fc809dd3f07c9a56e
payelbera <60781680+payelbera@users.noreply.github.com>
2022-01-03 20:34:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,52 @@
-from flask import Flask, jsonify,request
-app=Flask(__name__)
+from flask import Flask,jsonify, request
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
 """ @app.route("/")
 def hello_world():
-    return "hello world" """
+    return "Hello World!" """
 
-
-tasks=[
-    {
-        "id":1, "title":"buy groceries", "description":"milk,cheese,fruits","done":"False"
-    },
-     {
-        "id":2, "title":"learn python", "description":"complete all the projects","done":"False"
-    },
-]
-@app.route("/getdata")
-def gettask():
-    return jsonify({
-        "data":tasks
-    })
-
-@app.route("/adddata", methods=["POST"])
-def addtask():
+@app.route("/add-data", methods=["POST"])
+def add_task():
     if not request.json:
         return jsonify({
             "status":"error",
-            "message":"please provide the data"
+            "message": "Please provide the data!"
         },400)
 
-        task={
-            "id":tasks[-1]["id"]+1,
-            "title":request.json["title"],
-            "description":request.json.get("description",""),
-            "done":False
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({
+        "status":"success",
+        "message": "Task added succesfully!"
+    })
+    
 
-        }
-        tasks.append(task)
-        return jsonify({
-            "status":"success",
-            "message":"task added successfully"
-        })
-if(__name__=="__main__"):
+@app.route("/get-data")
+def get_task():
+    return jsonify({
+        "data" : tasks
+    }) 
+
+if (__name__ == "__main__"):
     app.run(debug=True)
